fe914eb570deaabf9a66fd09d2a26c20786894af
Garvit <garvitg0108@gmail.com>
2024-06-14 18:15:22 +0530

    third commit

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ const io = socektio(expressServer, {
         methods: ["GET", "POST"]
     }
 })
-
-expressServer.listen(3000, () => {
+const port = process.env.PORT || 3000;
+expressServer.listen(port, () => {
     console.log('Server is running on https://localhost:3000');
 });
 
